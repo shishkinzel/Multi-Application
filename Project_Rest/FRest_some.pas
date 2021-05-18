de7c158e599372cac4ae548ce772a350de44e0c8
shishkinzel <shishkin_angtel@rambler.ru>
2021-05-18 16:57:49 +0300

    Каркас приложения Rest_some

--- Project_Rest/FRest_some.pas
+++ Project_Rest/FRest_some.pas
@@ -5,7 +5,8 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Controls.Presentation,
-  FMX.StdCtrls, FMX.TabControl, FMX.Edit, FMX.ComboEdit, FMX.DateTimeCtrls;
+  FMX.StdCtrls, FMX.TabControl, FMX.Edit, FMX.ComboEdit, FMX.DateTimeCtrls,
+  FMX.EditBox, FMX.NumberBox, FMX.SpinBox, System.Actions, FMX.ActnList;
 
 type
   TfrmRest = class(TForm)
@@ -23,19 +24,29 @@ type
     dtdt2: TDateEdit;
     edt1: TComboEdit;
     lbl4: TLabel;
-    DateEdit1: TDateEdit;
-    ComboEdit1: TComboEdit;
-    DateEdit2: TDateEdit;
+    dtdt3: TDateEdit;
+    dtdt4: TDateEdit;
     Button1: TButton;
     Button2: TButton;
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
+    spnbx1: TSpinBox;
+    btnMenu: TSpeedButton;
+    btnClose: TSpeedButton;
+    actlst1: TActionList;
+    procedure FormCreate(Sender: TObject);
+    procedure edt1Change(Sender: TObject);
+    procedure dtdt1Change(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure btn2Click(Sender: TObject);
   private
     { Private declarations }
+    fday : Integer;
   public
     { Public declarations }
+    property day : Integer read fday write fday;
   end;
 
 var
@@ -45,4 +56,38 @@ implementation
 
 {$R *.fmx}
 
+procedure TfrmRest.btn2Click(Sender: TObject);
+begin
+frmRest.Close;
+end;
+
+procedure TfrmRest.Button2Click(Sender: TObject);
+begin
+dtdt4.Date := dtdt3.Date + spnbx1.Value;
+end;
+
+procedure TfrmRest.dtdt1Change(Sender: TObject);
+begin
+  day := 0;
+  edt1.ItemIndex := 0;
+end;
+
+procedure TfrmRest.edt1Change(Sender: TObject);
+begin
+  day := StrToIntDef(edt1.Text, 1);
+  dtdt2.Date := dtdt1.Date - (day - 1);
+
+  dtdt2.Visible := True;
+  lbl4.Visible := True;
+end;
+
+procedure TfrmRest.FormCreate(Sender: TObject);
+var
+  i: Integer;
+begin
+  dtdt1.Date := Now;
+  dtdt3.Date := Now;
+end;
+
 end.
+
