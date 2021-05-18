19ee221b3c8fde7b596195ae0be5c2042d5cc3f3
shishkinzel <shishkin_angtel@rambler.ru>
2021-05-18 13:20:30 +0300

    Заполненая форма

--- Project_Rest/FRest_some.pas
+++ Project_Rest/FRest_some.pas
@@ -5,7 +5,7 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Controls.Presentation,
-  FMX.StdCtrls, FMX.TabControl;
+  FMX.StdCtrls, FMX.TabControl, FMX.Edit, FMX.ComboEdit, FMX.DateTimeCtrls;
 
 type
   TfrmRest = class(TForm)
@@ -14,6 +14,24 @@ type
     tbtmHotell: TTabItem;
     tlbUp: TToolBar;
     tlbDown: TToolBar;
+    lblTitleOne: TLabel;
+    lblOut: TLabel;
+    lbl2: TLabel;
+    btn1: TButton;
+    btn2: TButton;
+    dtdt1: TDateEdit;
+    dtdt2: TDateEdit;
+    edt1: TComboEdit;
+    lbl4: TLabel;
+    DateEdit1: TDateEdit;
+    ComboEdit1: TComboEdit;
+    DateEdit2: TDateEdit;
+    Button1: TButton;
+    Button2: TButton;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
   private
     { Private declarations }
   public
